7bc45511b07e18450d8015b2e8824f13c0db1fce
Rafik Cherrak <cherrakrafik8@gmail.com>
2023-12-08 04:22:17 +1300

    added new connectiong with data base.

--- src/app.js
+++ src/app.js
@@ -1,46 +1,33 @@
+require('dotenv').config(); // Charge les variables d'environnement depuis le fichier .env
+
 const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorsHandling');
 const config = require('./config');
 const routes = require('./routes');
+const { MongoClient } = require('mongodb'); // Import du client MongoDB
 
 const app = express();
 
-// parse json request body
-app.use(express.json());
+// Récupère la chaîne de connexion MongoDB depuis les variables d'environnement
+const mongoURI = process.env.MONGODB_URI;
 
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
 
-// cors
-app.use(
-    cors(
-        {
-            origin: config.frontend_url,
-        },
-    ),
-);
+// Connexion à la base de données MongoDB
+MongoClient.connect(mongoURI)
+  .then((client) => {
+    const db = client.db(); // Utilisation de la base de données par défaut
 
-//access to public folder
-app.use(express.static(__dirname + '/public'));
+    // Injecter la connexion MongoDB dans l'application
+    app.locals.db = db;
+    console.error('connecting Bien Vu');
 
-// initial route
-app.get('/', (req, res) => {
-    res.send({ message: 'Welcome to app-store-api application.' });
-});
-
-// api routes prefix
-app.use(
-    '/api',
-    routes,
-);
-
-// error handling
-app.use(errorHandler);
-
-// run server
-app.listen(config.port, () => {
-    console.log('server launch');
-});
+    // Reste du code pour la configuration de votre serveur Express
+    // ...
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    // Gérer les erreurs de connexion ici
+  });
 
 module.exports = app;
