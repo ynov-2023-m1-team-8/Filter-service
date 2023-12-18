3b985f2cbc22fe9e59092c2f333dc1c95b6b6666
yi3444 <yasmine.ibrahimi@ynov.com>
2023-12-18 19:11:43 +0100

    save work

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,55 +3,7 @@ const { PrismaClient } = require('@prisma/client');
 const throwError = require('../utils/throwError');
 
 
-/*exports.getFilteredProducts = async (req, res, next) => {
-  try {
-    const { min, max } = req.query;
-    const products = await app.locals.db.collection('Products').find({
-      price: {
-        $gte: parseInt(min), // greater than or equal to (>=)
-        $lte: parseInt(max), // less than or equal to (<=)
-      },
-    }).toArray();
 
-    if (!products || products.length === 0) {
-      const err = throwError('No products found for the given price range', 404);
-      return next(err);
-    }
-
-    // Log the filter choice for analytics (anonymously)
-    const filterChoice = { min, max };
-    console.log('Filter Choice:', filterChoice);
-
-    // Store the filter choice in MongoDB 
-    const analyticsCollection = app.locals.db.collection('FilterAnalytics');
-    await analyticsCollection.insertOne(filterChoice);
-
-    return res.json({
-      success: true,
-      data: products,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
-
-exports.getFilterMetrics = async (req, res, next) => {
-  try {
-    // Use MongoDB aggregation to calculate metrics
-    const analyticsCollection = app.locals.db.collection('FilterAnalytics');
-    const metrics = await analyticsCollection.aggregate([
-      // Add your aggregation stages here
-      // Example: { $group: { _id: null, avgMin: { $avg: '$min' } } }
-    ]).toArray();
-
-    return res.json({
-      success: true,
-      data: metrics,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};*/
 
 exports.postFilter = async (req, res, next) => {
   try {
